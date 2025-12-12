7268041fe2dca0bc7c41a8e9be060923e065e797
FabianMessy <fabianmessy.90@gmail.com>
2025-12-12 16:45:18 -0300

    feat: adiciona rota inicial no controller e estrutura da tabela feita no model. todo: get e setters no model

--- cursos-api/src/main/java/com/example/cursos_api/controllers/cursoController.java
+++ cursos-api/src/main/java/com/example/cursos_api/controllers/cursoController.java
@@ -1,1 +1,17 @@
 package com.example.cursos_api.controllers;
+
+import com.example.cursos_api.models.cursoModel;
+import com.example.cursos_api.services.cursoService;
+
+import java.util.*;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+@RequestMapping("/home")
+public class cursoController{
+
+}
--- cursos-api/src/main/java/com/example/cursos_api/models/cursoModel.java
+++ cursos-api/src/main/java/com/example/cursos_api/models/cursoModel.java
@@ -1,1 +1,38 @@
 package com.example.cursos_api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "cursos")
+
+public class cursoModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+
+    private String nome;
+
+    private String categoria;
+
+    private Integer cargaHoraria;
+
+    
+    public cursoModel(
+        String nome, 
+        String categoria, 
+        Integer cargaHoraria
+    ){
+        this.nome = nome;
+        this.categoria = categoria;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public long getId() {return id;}
+}
